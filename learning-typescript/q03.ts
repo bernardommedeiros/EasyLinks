07ca12ec722d49cab451ea7ab7ea10b9568d7b51
Bernardo de Moura Medeiros <bernardo181105@gmail.com>
2025-10-05 21:19:03 +0000

    feat: q03 problem solution

---
+++ learning-typescript/q03.ts
@@ -0,0 +1,13 @@
+/* Faça um programa que imprima seus elementos ordenados. Use o método SORT da classe Array para ordenar de forma decrescente, passando uma função arrow como parâmetro.
+Escreva um teste com o array [‘carro’, ’boneco’, ’ave’, ‘lapis’] */
+
+let arr: string[] = ['carro', 'boneco', 'ave', 'lapis']
+
+const arrOrder = (arr: string[]) => {
+  return arr.sort( (a: string, b: string)=> {
+    if (a > b) return -1;
+    return 0;
+  });
+};
+
+console.log(arrOrder(arr))
